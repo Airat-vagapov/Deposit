9ad57814432a455d39fded79f8e8ff73866a7517
Airat-vagapov <airlake@yandex.ru>
2020-10-15 00:17:15 +0300

    Add deposit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    int sum = 1_000;
+    int percent = 3;
+    int profit = percent * sum / 100;
+    System.out.println(profit);
+        }
+    }
